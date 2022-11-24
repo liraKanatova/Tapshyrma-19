bb0d3af064a34dd066dd87c206119217c661fc8e
LiraKanatova <lira.kanaatova.ch@gmail.com>
2022-11-25 02:17:54 +0600

    first

--- src/Week.java
+++ src/Week.java
@@ -1,3 +1,24 @@
 public enum Week {
-    MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY
+    MONDAY("Дуйшөмбү куну жава окуйм"),
+    TUESDAY("Шейшемби күнү англис тили окуйм"),
+    WEDNESDAY("Шаршенби куну софт скиллс окуйм "),
+    THURSDAY("Бейшенби куну жава сабагынан практика кылам"),
+    FRIDAY("Жума куну англис тилин практика кылам"),
+    SATURDAY("Ишенби куну бардык сабактарынын уй ишин жасайм "),
+    SUNDAY("Жекшенби куну сабактарды кайталайм ");
+
+    private String timeTable;
+
+    Week(String timeTable) {
+        this.timeTable = timeTable;
+    }
+
+    public String getTimeTable() {
+        return timeTable;
+    }
+
+    public void setTimeTable(String timeTable) {
+        this.timeTable = timeTable;
+    }
+
 }
